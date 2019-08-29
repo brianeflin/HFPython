e6106663cd2c3d3080f79114d4b8b1afbafc6290
brianeflin <brian.eflin@gmail.com>
2019-08-28 21:11:28 -0500

    Initial commit

---
+++ ch01/odd2.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import time
+import random
+
+odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+
+    if right_this_minute in odds:
+        print(right_this_minute, "is very odd.")
+    else:
+        print(right_this_minute, "is not odd at all.")
+
+    if i < 4:
+        wait_time = random.randint(1, 60)
+        print("Let's check again in", wait_time, "seconds.")
+        time.sleep(wait_time)
